f7ff6e6558a62563a50b3b620620caa01cf57854
Ryan Kotval <ryan.kotval@gmail.com>
2013-06-09 22:25:39 -0500

    More framework files

--- endlessbowhunter/Animal.h
+++ endlessbowhunter/Animal.h
@@ -1,13 +1,13 @@
 
 #include <SFML/Graphics.hpp>
 
-class Animal {
+class Animal : public sf::Sprite {
 public:
 	Animal() {};
 	Animal(sf::Vector2f p, int h, int s, int t)
 		: position(p), health(h), speed(s), type(t), firstCall(true) {};
 	~Animal() {};
-	void move(sf::Vector2f){};
+	void move(sf::Vector2f);
 	int health;
 	int type;
 	int speed;
